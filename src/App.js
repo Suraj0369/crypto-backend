7a3fb5e288989cf1b47f5000e6d6467e5b2c98dd
RohithBoppey <rohithboppey1298@gmail.com>
2022-07-19 12:13:27 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import CoinPage from "./pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // Make styles is the hook from Material UI.
+  // We use a function to give us the styles and then use it again to directly return an object
+
+  const useStyles = makeStyles( () => (
+    {
+      App: {
+        backgroundColor: '#14161a',
+        color: 'white',
+        minHeight: '100vh'
+      }
+    }
+  ));
+
+  const classes = useStyles();
+  
+	return (
+		<div className={classes.App}>
+			<Header></Header>
+			<Routes>
+				<Route path="/" element={<Homepage />} exact/>
+				<Route path="/coins/:id" element={<CoinPage/>} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
